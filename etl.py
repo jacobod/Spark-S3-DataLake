b0c9641803787f310d92f968b7ba6a1775f55634
Jacob Dodd <jacobdodd94@gmail.com>
2019-07-01 21:58:57 -0700

    Update ETL script

--- etl.py
+++ etl.py
@@ -1,6 +1,8 @@
 import configparser
 from datetime import datetime
 import os
+from pyspark import SparkContext
+from pyspark import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
@@ -12,7 +14,6 @@ config.read('dl.cfg')
 os.environ['AWS_ACCESS_KEY_ID']=config['Default']['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['Default']['AWS_SECRET_ACCESS_KEY']
 
-
 def create_spark_session():
     spark = SparkSession \
         .builder \
@@ -23,37 +24,40 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = input_data +"/*.json"    #"s3://udacity-dend/song_data/*.json"
+    song_data = input_data    #"s3://udacity-dend/song_data/*.json"
 
     # read song data file
+    #sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
+    #text_rdd = sc.textFile(os.path.join(input_data, "*"))
     df = spark.read.json(song_data)
+    df.printSchema()
 
     # create global view for querying
-    df.createGlobalTempView("songs")
+    #df.createGlobalTempView("songs_ext")
 
     # extract columns to create songs table
-    songs_table = spark.select("""
-        SELECT song_id,
-               title,
-               arist_id,
-               year,
-               duration
-        FROM songs""")
+    #songs_table = spark.sql("""
+    #    SELECT song_id,
+    #           title,
+    #           arist_id,
+    #           year,
+    #           duration
+    #    FROM songs_ext""")
     # or if python
-    #songs_table = df.select(song_id,title,arist_id,year,duration)
+    songs_table = df.select("song_id","title","artist_id",'year',"duration")
 
     # write songs table to parquet files partitioned by year and artist
     songs_table.write.partitionBy("year", "artist_id").parquet(output_data+"/songs_table.parquet")
 
     #artist_id, name, location, lattitude, longitude
     # extract columns to create artists table
-    artists_table = spark.select("""
-        SELECT artist_id,
-               artist_name AS name,
-               artist_location AS location,
-               artist_lattitude AS lattitude,
-               artist_longitude AS longitude
-        FROM songs""")
+    #artists_table = spark.sql("""
+    #    SELECT artist_id,
+    #           artist_name AS name,
+    #           artist_location AS location,
+    #           artist_lattitude AS lattitude,
+    #           artist_longitude AS longitude
+    #    FROM songs""")
     artists_table = artists_table.drop_duplicates()
 
     # write artists table to parquet files
@@ -67,7 +71,7 @@ def get_datetime(ts):
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data = input_data +"/*.json" #"s3://udacity-dend/log_data"
+    log_data = input_data  #"s3://udacity-dend/log_data"
 
     # read log data file
     df = spark.read.json(log_data)
@@ -123,7 +127,7 @@ def process_log_data(spark, input_data, output_data):
     time_table.write.parquet(output_data+"/time_table.parquet")
 
     # read in song data to use for songplays table
-    song_df = "s3://udacity-dend/song_data/*.json"
+    song_df = spark.read.json("./Data/song_data/")
 
     #songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent
     # extract columns from joined song and log datasets to create songplays table
@@ -153,11 +157,11 @@ def process_log_data(spark, input_data, output_data):
 
 def main():
     spark = create_spark_session()
-    input_data = "data/" #"s3a://udacity-dend/"
+    input_data = "./Data/" #"s3a://udacity-dend/"
     output_data = "Output"
 
-    process_song_data(spark, input_data, output_data)
-    process_log_data(spark, input_data, output_data)
+    process_song_data(spark, input_data+"song_data/A/A/A/TRAAAAW128F429D538.json", output_data) #Documents/UdacityDataEngineering/SparkProject/Data/song_data/A/A/A/TRAAAAW128F429D538.json
+    process_log_data(spark, input_data+"log_data", output_data)
 
 
 if __name__ == "__main__":
